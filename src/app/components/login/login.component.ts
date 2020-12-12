a06e1f2832f96bcf7b15feae2103607377bd394b
Daniel Iovescu <iovescu.dani@gmail.com>
2020-12-12 19:51:26 +0200

    icons mobile & desktop view

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormGroup} from '@angular/forms';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,16 @@ import {FormGroup} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   authForm: FormGroup;
+  isMobile: boolean;
+  isDesktop: boolean;
 
-  constructor() {
+  constructor(private deviceDetector: DeviceDetectorService) {
   }
 
   ngOnInit(): void {
+    this.isMobile = this.deviceDetector.isMobile();
+    this.isDesktop = this.deviceDetector.isDesktop();
+
     this.authForm = new FormGroup({});
   }
 
